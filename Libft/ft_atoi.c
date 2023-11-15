86dadc317ccc8065ddcbc4a93b381eee7072f340
Sabli Jr <sablijr@yahoo.com>
2023-11-15 06:33:20 +0100

    Finishing the first collection of functions

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,28 @@
+#include  "libft.h"
+
+int ft_atoi (const char *str)
+{
+  int i;
+  int res;
+  int sign;
+
+  i = 0;
+  res = 0;
+  sign = 1;
+
+  while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+    i++;
+
+  if (str[i] == '-')
+  {
+    sign = -1;
+    i++;
+  }
+
+  while ((str[i] >= '0' && str[i] <= '9') || (str[i] != '\0'))
+  {
+    res = res * 10 + str[i] - '0';
+    i++;
+  }
+  return (res * sign);
+}
