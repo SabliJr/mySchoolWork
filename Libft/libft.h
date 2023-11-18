39733da3a8c1b3d677ac99271fb9fe9550639103
Sabli Jr <sablijr@yahoo.com>
2023-11-18 11:56:14 +0100

    Checking the functions

--- Libft/libft.h
+++ Libft/libft.h
@@ -25,7 +25,7 @@ char *ft_strchr (const char *str, int c);
 char  *ft_strrchr (const char *str, int c);
 int ft_strncmp (const char *str1, const char *str2, size_t n);
 void  *ft_memchr (const void *str, int c, size_t n);
-int ft_memcmp (const void *str1, const char *str2, size_t n);
+int ft_memcmp (const void *str1, const void *str2, size_t n);
 char  *ft_strnstr (const char *haystack, const char *needle, size_t len);
 int ft_atoi (const char *str);
 void	*ft_calloc (size_t nmemb, size_t size);
