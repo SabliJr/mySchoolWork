9f2fb91a2cbb1bae4c4ede339fe76f882fa216be
Sabli Junior Abakar Arbab <sabakar-@bess-f1r6s2.clusters.42paris.fr>
2023-11-21 11:35:39 +0100

    Adding the makefile

--- Libft/libft.h
+++ Libft/libft.h
@@ -47,5 +47,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 size_t				ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t len);
 size_t	ft_strlcat(char *dst, const char *src, size_t len);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int	ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstdelone(tlist *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *0));
 
 #endif
